979cb9d8d4c62e335a62a5c90ba5ed1be4b2e7d1
Bruno França Souza <brunofrancasouza@gmail.com>
2019-03-08 21:55:11 -0300

    Add BootStrap

--- Assistencia-Tecnica-app/src/app/app.module.ts
+++ Assistencia-Tecnica-app/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient } from 'selenium-webdriver/http';
+
+import { HttpClientModule } from '@angular/common/http';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      UsuarioComponent
+   ],
+   imports: [
+      AppRoutingModule,
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ Assistencia-Tecnica-app/src/app/usuario/usuario.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { debug } from 'util';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css']
+})
+export class UsuarioComponent implements OnInit {
+
+  constructor( private http: HttpClient ) { }
+
+  Usuarios: any;
+
+  ngOnInit() {
+    this.getUsuarios();
+  }
+
+  getUsuarios() {
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      response => { this.Usuarios = response; },
+      error => { console.log(error); }
+      );
+  }
+
+}
